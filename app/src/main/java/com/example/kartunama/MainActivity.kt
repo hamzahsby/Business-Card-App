23dd55b806b013779da698163d863810f9d0ae82
hamzahsby <hamzah.sby@gmail.com>
2022-11-25 21:09:58 +0700

    Membuat fungsi IdentityBlock()

--- app/src/main/java/com/example/kartunama/MainActivity.kt
+++ app/src/main/java/com/example/kartunama/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.kartunama
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.kartunama.ui.theme.KartuNamaTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,6 +36,33 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+private fun IdentityBlock() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        val image = painterResource(R.drawable.android_logo)
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.size(width = 100.dp , height = 100.dp)
+        )
+        Text(
+            text = "Jennifer Doe",
+            color = Color.White,
+            fontSize = 45.sp,
+            modifier = Modifier.padding(bottom = 10.dp)
+        )
+        Text(
+            text = "Android Developer Extraordinaire",
+            color = Color(0xFF3ddc84),
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
